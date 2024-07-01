39206be3df4fb1bf022bf0de0e1b0de37deeefa2
Seb <121176289+sebDtSci@users.noreply.github.com>
2024-07-01 09:47:58 +0200

    last mod

--- TP-Syracuse/src/main/java/drools/KogitoApplication.java
+++ TP-Syracuse/src/main/java/drools/KogitoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = { "drools.**", "org.kie.kogito.**", "http**" })
+@SpringBootApplication(scanBasePackages = { "rules.**", "org.kie.kogito.**", "http**" })
 public class KogitoApplication {
     public static void main(String[] args) {
         SpringApplication.run(KogitoApplication.class, args);
