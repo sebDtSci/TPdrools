acb9f21a95776df7d3e7a89e0b77318943aca13a
sebDtSci <sebastientadiello@gmail.com>
2024-07-02 11:36:25 +0200

    running

--- TP-Syracuse/src/main/java/rules/SyracuseUnit.java
+++ TP-Syracuse/src/main/java/rules/SyracuseUnit.java
@@ -1,17 +1,19 @@
 package rules;
 
 import org.drools.ruleunits.api.DataSource;
+import org.drools.ruleunits.api.DataStore;
 import org.drools.ruleunits.api.RuleUnitData;
 import org.drools.ruleunits.api.SingletonStore;
 
 import drools.SyracuseData;
 
 public class SyracuseUnit implements RuleUnitData {
-    private DataSource<SyracuseData> syracuseData;
+    // private DataSource<SyracuseData> syracuseData;
+    private DataStore<SyracuseData> syracuseData;
     private SingletonStore<Integer> startValue;
 
     public SyracuseUnit() {
-        this.syracuseData = DataSource.createSingleton();
+        this.syracuseData = DataSource.createStore();
         this.startValue = DataSource.createSingleton();
     }
 
@@ -28,7 +30,7 @@ public class SyracuseUnit implements RuleUnitData {
         return syracuseData;
     }
 
-    public void setSyracuseData(DataSource<SyracuseData> syracuseData) {
+    public void setSyracuseData(DataStore<SyracuseData> syracuseData) {
         this.syracuseData = syracuseData;
     }
 }
